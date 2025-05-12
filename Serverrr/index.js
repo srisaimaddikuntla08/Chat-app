4f871c9b4b02b9a4a4c9204341c110fa8d37965a
srisai810 <srisaimaddikuntlams@gmail.com>
2025-05-12 17:54:14 +0530

    next commit message

--- Serverrr/index.js
+++ Serverrr/index.js
@@ -55,7 +55,9 @@ app.use("/api/messages/",messageRouter)
 await connectDB()
 
 
+if(process.env.NODE_ENV!=="production"){
 const PORT = process.env.PORT||5000
-
-
 server.listen(PORT,()=>console.log(`server is running on port:${PORT}`))
+}
+
+export default server;
