15f2b354a9293fc3dbf7f597b0a805e538dbc66f
srisai810 <srisaimaddikuntlams@gmail.com>
2025-05-12 20:20:01 +0530

    b

--- Cliennt/src/App.jsx
+++ Cliennt/src/App.jsx
@@ -8,10 +8,11 @@ import {AuthContext} from "../AuthContext";
 
 
 
+
 function App() {
     const {authUser} = useContext(AuthContext)
   return (
-    <div className="bg-[url('/bgImage.svg')] bg-contain ">
+    <div className={`bg-[url(./assets/bgImage.svg)] bg-contain `}>
       <Toaster/>
       <Routes>
         <Route path="/" element={authUser ? <Home/> : <Navigate to ="/login"/>}/>
